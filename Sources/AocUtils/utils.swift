b1b611b64d3172b5fa53b1c08a9a34ffe6247e2f
Kenneth Jonsson <1.kenneth.jonsson@gmail.com>
2020-12-19 15:59:39 +0100

    Keep empty

--- Sources/AocUtils/utils.swift
+++ Sources/AocUtils/utils.swift
@@ -1,12 +1,12 @@
 import Dispatch
 import Foundation
 
-public func inputGet(separatedBy: String = "\n") -> [String] {
+public func inputGet(separatedBy: String = "\n", keepEmpty: Bool = false) -> [String] {
     let fileName = CommandLine.argc == 1 ? "input" : CommandLine.arguments[1]
     let cwdURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
     let fileURL = URL(fileURLWithPath: fileName, relativeTo: cwdURL).appendingPathExtension("txt")
     let content = try! String(contentsOf: fileURL, encoding: String.Encoding.utf8)
-    return content.components(separatedBy: separatedBy).filter { !$0.isEmpty }
+    return content.components(separatedBy: separatedBy).filter { keepEmpty || !$0.isEmpty }
 }
 
 public func recordsGet() -> [String] {
